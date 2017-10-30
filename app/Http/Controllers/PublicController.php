149f40591355be3dde96c00d0d06a6dff076ff70
Carlos Mendez <carlos@hadaelectronics.com>
2017-10-30 15:03:45 -0600

    fixes

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -1461,39 +1461,39 @@ class PublicController extends Controller
 
                 if($ask->user->type == 'soloist')
                 {
-                    // $info = User_info::select('slug')->where('user_id', $ask->user_id)->firstOrFail();
-                    // $data = [ 
-                    //     'id'      => $ask->user->id,
-                    //     'u_email' => $ask->user->email,
-                    //     'u_name'  => $ask->user->info->first_name.' '.$ask->user->info->last_name,
-                    //     'c_email' => $ask->email,
-                    //     'c_name'  => $ask->name,
-                    //     'price'   => $ask->price,
-                    //     'type'    => $payment->type,
-                    //     'amount'  => $payment->amount,
-                    //     'day'     => $start_date[1],
-                    //     'flag'    => $flag_client,
-                    // ];
+                    $information = User_info::select('slug')->where('user_id', $ask->user_id)->firstOrFail();
+                    $data = [ 
+                        'id'      => $ask->user->id,
+                        'u_email' => $ask->user->email,
+                        'u_name'  => $ask->user->info->first_name.' '.$ask->user->info->last_name,
+                        'c_email' => $ask->email,
+                        'c_name'  => $ask->name,
+                        'price'   => $ask->price,
+                        'type'    => $payment->type,
+                        'amount'  => $payment->amount,
+                        'day'     => $start_date[1],
+                        'flag'    => $flag_client,
+                    ];
                 }
-                // elseif($ask->user->type == 'ensemble') 
-                // {
-                //     $info = Ensemble::select('slug')->where('user_id', $ask->user_id)->firstOrFail();
-                //     $data = [ 
-                //         'id'      => $ask->user->id,
-                //         'u_email' => $ask->user->email,
-                //         'u_name'  => $ask->user->ensemble->name,
-                //         'c_email' => $ask->email,
-                //         'c_name'  => $ask->name,
-                //         'price'   => $ask->price,
-                //         'type'    => $payment->type,
-                //         'amount'  => $payment->amount,
-                //         'day'     => $start_date[1],
-                //         'flag'    => $flag_client,
-                //     ];
-                // }
+                elseif($ask->user->type == 'ensemble') 
+                {
+                    $information = Ensemble::select('slug')->where('user_id', $ask->user_id)->firstOrFail();
+                    $data = [ 
+                        'id'      => $ask->user->id,
+                        'u_email' => $ask->user->email,
+                        'u_name'  => $ask->user->ensemble->name,
+                        'c_email' => $ask->email,
+                        'c_name'  => $ask->name,
+                        'price'   => $ask->price,
+                        'type'    => $payment->type,
+                        'amount'  => $payment->amount,
+                        'day'     => $start_date[1],
+                        'flag'    => $flag_client,
+                    ];
+                }
 
                 // $this->SendMailApproved($data);
-                // $payment_object->slug = $info->slug;
+                // $payment_object->slug = $information->slug;
                 $payment_object->status ='OK';
                 $payment_object->message = "REDIRECTING---WE SEND YOU AN EMAIL WITH ALL THE INFORMATION---REDIRECTING";
             }catch(ServerErrorException $e) {
