52769aafbb2be61207e05dcebd400c8ca7f9237d
Carlos Mendez <carlos@hadaelectronics.com>
2017-10-30 15:31:17 -0600

    fixes

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -1459,18 +1459,17 @@ class PublicController extends Controller
                     'type'       => 'paypal',
                 ]);
 
-                    // $data = [ 
-                    //     'id'      => $ask->user->id,
-                    //     'u_email' => $ask->user->email,
-                    //     'u_name'  => $ask->user->info->first_name.' '.$ask->user->info->last_name,
-                    //     'c_email' => $ask->email,
-                    //     'c_name'  => $ask->name,
-                    //     'price'   => $ask->price,
-                    //     'type'    => $payment->type,
-                    //     'amount'  => $payment->amount,
-                    //     'day'     => $start_date[1],
-                    //     'flag'    => $flag_client,
-                    // ];
+                $flag_client = 0;
+                $data = ['id' => $ask->user->id,
+                         'u_email' => $ask->user->email,
+                         'u_name'  => 'musician',
+                         'c_email' => $ask->email,
+                         'c_name'  => $ask->name,
+                         'price'   => $ask->price,
+                         'type'    => $payment->type,
+                         'amount'  => $payment->amount,
+                         'day'     => $start_date[1],
+                         'flag'    => $flag_client];
 
                 // $this->SendMailApproved($data);
                 // $payment_object->slug = $information->slug;
